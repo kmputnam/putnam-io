ff7e11b3aa2476f3c871703381b6989b1296298f
Kyle Putnam <kyle@putnam.io>
2025-03-28 23:15:11 -0500

    idk what the hell i’m doing

---
+++ eleventy.config.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addCollection("blog", function(collectionApi) {
+	return collectionApi.getFilteredByGlob("./blog/*.md");
+  });
+  return {
+	dir: {
+	  input: ".",
+	  output: "_site"
+	}
+  };
+};
